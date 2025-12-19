3030dbebba1eec85a62aaadf82c412811779f5e7
DebaPrasadbehura424 <debaprasadbehura89@gmail.com>
2025-12-19 20:16:27 +0530

    re update

--- backend/app.js
+++ backend/app.js
@@ -6,10 +6,6 @@ import authRoutes from "./routes/auth.routes.js";
 // import propertyRoutes from "./routes/property.routes.js";
 // import tenantRoutes from "./routes/tenant.routes.js";
 // import paymentRoutes from "./routes/payment.routes.js";
-// import maintenanceRoutes from "./routes/maintenance.routes.js";
-// import fileRoutes from "./routes/file.routes.js";
-// import reminderRoutes from "./routes/reminder.routes.js";
-// import importRoutes from "./routes/import.routes.js";
 
 const app = express();
 
@@ -22,10 +18,6 @@ app.use("/api/auth", authRoutes);
 // app.use("/api/properties", propertyRoutes);
 // app.use("/api/tenant", tenantRoutes);
 // app.use("/api/payment", paymentRoutes);
-// app.use("/api/maintain", maintenanceRoutes);
-// app.use("/api/files", fileRoutes);
-// app.use("/api/reminders", reminderRoutes);
-// app.use("/api/import", importRoutes);
 
 const PORT = process.env.PORT || 5000;
 
